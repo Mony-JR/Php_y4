073ede06c3c56148f12e088000625b96c3c08f94
MooNyWindow <sreyloth97a@gmail.com>
2025-01-16 01:43:13 -0500

    f

--- v1/index.php
+++ v1/index.php
@@ -3,11 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP</title>
+    <title>PHP and MySQL Connection</title>
 </head>
 <body>
-    <a href="../week1.php"><button>Week2</button></a>
-   
+    <?php 
+    // MySQL connection
+    $host = 'localhost:3306';
+    $username = 'root';
+    $password = '';
+    $database = 'db_php';
 
+    $conn = new mysqli($host, $username, $password, $database);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+    echo "<p>Connected to MySQL successfully!</p>";
+    ?>
+    <a href="../week1.php"><button>Week2</button></a>
 </body>
 </html>
